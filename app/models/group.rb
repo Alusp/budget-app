4642c21bcc39b4f7726ce7698ea6114fee2248ee
Alusp <alusinejalloh800@gmail.com>
2022-12-23 09:00:36 +0200

    Add Group model

---
+++ app/models/group.rb
@@ -0,0 +1,8 @@
+class Group < ApplicationRecord
+  belongs_to :user
+  has_many :purchases, through: :group_purchases
+  has_many :group_purchases
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :icon, presence: true
+end
